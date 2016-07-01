25fc46a9042dceae7c478009dc5fc4118a778b45
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-06-30 18:26:48 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+if __name__== '__main__':
+    setup(include_package_data=True,
+          description='MOtif Discovery from Importance SCOres',
+          url='NA',
+          download_url='NA',
+          version='0.1',
+          packages=['modisco'],
+          setup_requires=[],
+          install_requires=['numpy>=1.9', 'theano>=0.8'],
+          scripts=[],
+          name='modisco')
