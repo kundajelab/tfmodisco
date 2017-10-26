45876662f92e1e803526824e0bf21059a54907ed
annashcherbina <annashch@stanford.edu>
2017-10-26 16:51:38 -0700

    html reports api skeleton

---
+++ modisco/visualization/html_reports.py
@@ -0,0 +1,30 @@
+class MetaclusterHeatmap(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+
+class VSeqlet(object):
+    """
+
+    """
+    def __init__(self,):
+        pass
+    
+
+class VTrack(object):
+    """
+    
+    """
+    def __init__(self,):
+        pass
+    
+class VHistogram(object):
+    """
+
+    """
+    def __init__(self,):
+        pass
+
+    
