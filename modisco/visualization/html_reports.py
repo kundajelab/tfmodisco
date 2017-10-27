d681a2eb7abac3eaf086542c910a3b7389d3a603
annashcherbina <annashch@stanford.edu>
2017-10-26 19:11:02 -0700

    steps toward api for html report generation

--- modisco/visualization/html_reports.py
+++ modisco/visualization/html_reports.py
@@ -1,30 +1,62 @@
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import numpy as np
+import plotly.plotly as py
+
+
+def load_image(image):
+    """
+    Determines whether 'image' is a string 
+    or a matplotlib figure handle. 
+    Loads the image accordingly
+    Returns a string with a '<div>' entity for embedding into html. 
+    """
+    return image 
+
+
+
 class MetaclusterHeatmap(object):
     """
+    Inputs:
+    image can be either a string to a png image file 
+    or a matplotlib figure handle 
     """
-    def __init__(self):
-        pass
+    def __init__(self,image):
+        self.image=load_image(image) 
+
 
 
 class VSeqlet(object):
     """
+    Inputs:
+    image can be either a string to a png image file 
+    or a matplotlib figure handle 
 
     """
-    def __init__(self,):
-        pass
+    def __init__(self,image):
+        self.image=load_image(image) 
     
 
 class VTrack(object):
     """
+    Inputs:
+    image can be either a string to a png image file 
+    or a matplotlib figure handle 
     
     """
-    def __init__(self,):
-        pass
+    def __init__(self,image):
+        self.image=load_imaeg(image) 
+
     
 class VHistogram(object):
     """
+    Inputs:
+    image can be either a string to a png image file 
+    or a matplotlib figure handle 
 
     """
-    def __init__(self,):
-        pass
+    def __init__(self,image):
+        self.image=load_image(image) 
 
     
+
