9016a64323f8e31e1debb6b3360beafcee385da4
annashcherbina <annashcherbina@gmail.com>
2017-10-29 23:33:19 -0700

    fixing deleted files and mockup html

---
+++ modisco/visualization/html/html_reports.py
@@ -0,0 +1,13 @@
+#Generates HTML report for MODISCO analysis.
+from html_class import *
+
+
+def generate_html_report(dataset,oprefix):
+        """
+    Inputs: 
+    dataset is an instance of VDataset object 
+    oprefix is a string indicating the output prefix for the html file to generate 
+    """
+    outf=open(oprefix+'.html','w')
+    pass
+            
