a873bd6a54349558f91200d047ded9d4c6dd3501
annashcherbina <annashcherbina@gmail.com>
2017-11-13 14:07:46 -0800

    periodic commit

--- modisco/visualization/html/spawn_html.py
+++ modisco/visualization/html/spawn_html.py
@@ -1,9 +1,55 @@
 from yattag import *
 from encode_js_functions import *
 
-def add_metacluster(doc,tag,metacluster):
-    pass
+def add_seqlet(doc,tag,cur_seqlet,seqlet_index,modal_image_function_calls):
+    
+    return modal_image_function_calls 
+def add_aggregate_motif(doc,tag,aggregate_motif,modal_image_function_calls):
+    return modal_image_function_calls
 
+def add_cluster(doc,tag,cur_cluster,cluster_index,modal_image_function_calls):
+    with tag('div',id='cluster_{cluster_index}'.format(cluster_index=cluster_index),
+             klass="tabcontent"):
+        modal_image_function_calls=add_aggregate_motif(doc,tag,cur_cluster.aggregate_motif,modal_image_function_calls)
+        with tag('div',klass='column'):
+            with tag('h3'):
+                text("TSNE highlighting cluster {cluster_index}".format(cluster_index=cluster_index))
+            modal_image_function_calls.append(add_modal_image(doc,tag,cur_cluster.tsne_embedding.image,id=len(modal_image_functions_calls)))
+
+        with tag('button',klass='accordion'):
+            text('Sample Seqlets')
+        with tag('div',klass='panel'):
+            for seqlet_index in range(len(cur_cluster.example_seqlets)):
+                cur_seqlet=cur_cluster.example_seqlets[seqlet_index]
+                modal_image_function_calls=add_seqlet(doc,tag,cur_seqlet,seqlet_index,modal_image_function_calls)
+    return modal_image_function_calls 
+        
+def add_metacluster(doc,tag,cur_metacluster,metacluster_index,modal_image_function_calls):
+    with tag('button',klass='accordion'):
+        text("Metacluster {metacluster_index}".format(metacluster_index=metacluster_index))
+    with tag('div',klass='panel'):
+        with tag('div',klass='tsne'):
+            #add a modal image for TSNE
+            modal_image_function_calls.append(add_modal_image(doc,tag,metacluster.tsne_embedding.image,id=len(modal_image_function_calls)))
+            with tag('p'):
+                text('TSNE Embedding')
+            #add a modal image for denoised TSNE
+            modal_image_function_calls.append(add_modal_image(doc,tag,metacluster.tsne_embedding_denoised,id=len(modal_image_function_calls)))
+            with tag('p'):
+                text('Noise-Filtered TSNE Embedding')
+                
+        #add cluster tabs for the current metacluster 
+        with tag('div',klass='tab'):
+            for cluster_index in range(range(len(cur_metacluster.clusters))):
+                with tag('button',klass='tablinks',onclick="openCluster(event,'cluste_{cluster_index})".format(cluster_index=cluster_index)):
+                    text("cluster_{cluster_index}".format(cluster_index=cluster_index))
+                    
+        #add a div containing cluster-specific information for each cluster within the metacluster 
+        for cluster_index in range(len(cur_metacluster.clusters)):
+            cur_cluster=cur_metacluster.clusters[cluster_index]
+            modal_image_function_calls=add_cluster(doc,tag,cur_cluster,cluster_index,modal_image_function_calls)
+            
+    return modal_image_function_calls 
 
 def add_per_task_histograms(doc,tag,vdataset):
     with tag('ul'):                    
@@ -104,10 +150,15 @@ def generate_html_string(vdataset):
                 with tag('h2'):
                     text("Task-specific histograms")
                 #add the per-task histograms
-                add_per_task_histograms(doc,tag,vdataset)            
+                add_per_task_histograms(doc,tag,vdataset)
+                
+        #add each of the metaclusters                 
+        for metacluster_index  in range(len(vdataset.metaclusters)):
+            cur_metacluster=vdataset.metaclusters[metacluster_index]
+            modal_image_function_calls=add_metacluster(doc,tag,cur_metacluster,metacluster_index,modal_image_function_calls)
+            
                         
-        for metacluster in vdataset.metaclusters:
-            add_metacluster(doc,tag,metacluster) 
+
         #add the scripts
         add_sripts(doc,tag,modal_image_function_calls)
             
@@ -119,6 +170,7 @@ if __name__=="__main__":
     import sys
     sys.path.append('tests')
     from generate_vdataset import *
+    
     vdataset=generate_vdataset_from_folder("/home/annashch/modisco_private/modisco/visualization/html/example_figures_modisco/")
     test_out=generate_html_string(vdataset)
     outf=open('test_small.html','w')
