22690500b66e30d02ae8122dc4768cf16d4568d1
Av Shrikumar <avanti.shrikumar@gmail.com>
2017-11-25 23:50:52 -0700

    Added modisco.backend to setup.py
    Thanks to anna for spotting the issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__== '__main__':
           url='NA',
           download_url='NA',
           version='0.1',
-          packages=['modisco', 'modisco.cluster',
+          packages=['modisco', 'modisco.cluster','modisco.backend',
                     'modisco.visualization', 'modisco.affinitymat'],
           setup_requires=[],
           install_requires=['numpy>=1.9', 'theano>=0.9', 'scikit-learn>=0.19'],
