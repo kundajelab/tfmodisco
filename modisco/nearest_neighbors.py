bc763a340910f8ada61b5b6563f5a2a370736caf
xrm2 <xrm2@cornell.edu>
2019-05-23 10:45:08 -0700

    prevent negative values from being passed to pairwise distances

--- modisco/nearest_neighbors.py
+++ modisco/nearest_neighbors.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, absolute_import
 from sklearn.neighbors import NearestNeighbors
-
+import numpy as np
 
 class AbstractNearestNeighborsComputer(object):
 
@@ -13,13 +13,9 @@ class ScikitNearestNeighbors(AbstractNearestNeighborsComputer):
     def __init__(self, n_neighbors, nn_n_jobs):
         self.n_neighbors = n_neighbors
         self.nn_n_jobs = nn_n_jobs
-        self.nn_object = NearestNeighbors( 
-                            algorithm="brute", metric="precomputed",
-                            n_jobs=self.nn_n_jobs)
+        self.nn_object = NearestNeighbors(
+            algorithm="brute", metric="precomputed",
+            n_jobs=self.nn_n_jobs)
 
     def __call__(self, affinity_mat):
-        return self.nn_object.fit(-affinity_mat).kneighbors(
-                X=-affinity_mat,
-                n_neighbors=min(self.n_neighbors+1,
-                                len(affinity_mat)),
-                return_distance=False)
+        return self.nn_object.fit(np.max(affinity_mat) - affinity_mat).kneighbors(X=np.max(affinity_mat) - affinity_mat,                                                                                                                        n_neighbors=min(self.n_neighbors+1,                                                                                                                           len(affinity_mat)), return_distance=False)
