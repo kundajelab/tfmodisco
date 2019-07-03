22bf1184cee91960126e53c6bbc5a03e427fed76
Av Shrikumar <avanti.shrikumar@gmail.com>
2019-07-03 15:56:32 -0700

    Fixing potential div by 0 error

--- modisco/affinitymat/core.py
+++ modisco/affinitymat/core.py
@@ -267,7 +267,10 @@ def contin_jaccard_vec_mat_sim(a_row, mat):
                                      np.abs(mat))
                           *np.sign(a_row[None,:])
                           *np.sign(mat), axis=1)
-    return intersection.astype("float")/union
+    if (union==0.0):
+        return 0
+    else:
+        return intersection.astype("float")/union
 
 
 class ContinJaccardSimilarity(AbstractAffinityMatrixFromOneD):
