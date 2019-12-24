1fb2be4da1b53cb7efdf71d433c144070ec2d6a0
Av Shrikumar <avanti.shrikumar@gmail.com>
2019-12-24 13:26:27 -0800

    updated nb

--- modisco/cluster/core.py
+++ modisco/cluster/core.py
@@ -49,34 +49,6 @@ class AbstractAffinityMatClusterer(object):
 
     def __call__(self, affinity_mat):
         raise NotImplementedError()
-
-
-class PhenographCluster(AbstractAffinityMatClusterer):
-
-    def __init__(self, k=30, min_cluster_size=10, jaccard=True,
-                       primary_metric='euclidean',
-                       n_jobs=-1, q_tol=0.0, louvain_time_limit=2000,
-                       nn_method='kdtree'):
-        self.k = k
-        self.min_cluster_size = min_cluster_size
-        self.jaccard = jaccard
-        self.primary_metric = primary_metric
-        self.n_jobs = n_jobs
-        self.q_tol = q_tol
-        self.louvain_time_limit = louvain_time_limit
-        self.nn_method = nn_method
-    
-    def __call__(self, affinity_mat):
-        communities, graph, Q, = ph.cluster.cluster(
-            data=affinity_mat,
-            k=self.k, min_cluster_size=self.min_cluster_size,
-            jaccard=self.jaccard, primary_metric=self.primary_metric,
-            n_jobs=self.n_jobs, q_tol=self.q_tol,
-            louvain_time_limit=self.louvain_time_limit,
-            nn_method=self.nn_method)
-        return LouvainClusterResults(
-                cluster_indices=communities,
-                Q=Q)
         
 
 class LouvainCluster(AbstractAffinityMatClusterer):
@@ -263,4 +235,3 @@ class CollectComponents(AbstractAffinityMatClusterer):
 
         return ClusterResults(cluster_indices=cluster_indices,
                     distinct_sets=distinct_sets)
-
--- modisco/cluster/phenograph/cluster.py
+++ modisco/cluster/phenograph/cluster.py
@@ -32,97 +32,11 @@ def sort_by_size(clusters, min_size):
     return relabeled
 
 
-def cluster(data, 
-            k=30, directed=False, prune=False, min_cluster_size=10, jaccard=True,
-            primary_metric='euclidean', n_jobs=-1, q_tol=1e-3, louvain_time_limit=2000,
-            nn_method='kdtree'):
-    """
-    PhenoGraph clustering
-    :param data: Numpy ndarray of data to cluster, or sparse matrix of k-nearest neighbor graph
-        If ndarray, n-by-d array of n cells in d dimensions
-        If sparse matrix, n-by-n adjacency matrix
-    :param k: Number of nearest neighbors to use in first step of graph construction
-    :param directed: Whether to use a symmetric (default) or asymmetric ("directed") graph
-        The graph construction process produces a directed graph, which is symmetrized by one of two methods (see below)
-    :param prune: Whether to symmetrize by taking the average (prune=False) or product (prune=True) between the graph
-        and its transpose
-    :param min_cluster_size: Cells that end up in a cluster smaller than min_cluster_size are considered outliers
-        and are assigned to -1 in the cluster labels
-    :param jaccard: If True, use Jaccard metric between k-neighborhoods to build graph.
-        If False, use a Gaussian kernel.
-    :param primary_metric: Distance metric to define nearest neighbors.
-        Options include: {'euclidean', 'manhattan', 'correlation', 'cosine'}
-        Note that performance will be slower for correlation and cosine.
-    :param n_jobs: Nearest Neighbors and Jaccard coefficients will be computed in parallel using n_jobs. If n_jobs=-1,
-        the number of jobs is determined automatically
-    :param q_tol: Tolerance (i.e., precision) for monitoring modularity optimization
-    :param louvain_time_limit: Maximum number of seconds to run modularity optimization. If exceeded
-        the best result so far is returned
-    :param nn_method: Whether to use brute force or kdtree for nearest neighbor search. For very large high-dimensional
-        data sets, brute force (with parallel computation) performs faster than kdtree.
-    :return communities: numpy integer array of community assignments for each row in data
-    :return graph: numpy sparse array of the graph that was used for clustering
-    :return Q: the modularity score for communities on graph
-    """
-
-    # NB if prune=True, graph must be undirected, and the prune setting takes precedence
-    if prune:
-        print("Setting directed=False because prune=True")
-        directed = False
-
-    if n_jobs == 1:
-        kernel = jaccard_kernel
-    else:
-        kernel = parallel_jaccard_kernel
-    kernelargs = {}
-
-    # Start timer
-    tic = time.time()
-    # Go!
-    if isinstance(data, sp.spmatrix) and data.shape[0] == data.shape[1]:
-        print("Using neighbor information from provided graph, rather than computing neighbors directly")
-        lilmatrix = data.tolil()
-        d = np.vstack(lilmatrix.data).astype('float32')  # distances
-        idx = np.vstack(lilmatrix.rows).astype('int32')  # neighbor indices by row
-        del lilmatrix
-        assert idx.shape[0] == data.shape[0]
-        k = idx.shape[1]
-    else:
-        d, idx = find_neighbors(data, k=k, metric=primary_metric, method=nn_method, n_jobs=n_jobs)
-        print("Neighbors computed in {} seconds".format(time.time() - tic))
-
-    subtic = time.time()
-    kernelargs['idx'] = idx
-    # if not using jaccard kernel, use gaussian
-    if not jaccard:
-        kernelargs['d'] = d
-        kernelargs['sigma'] = 1.
-        kernel = gaussian_kernel
-        graph = neighbor_graph(kernel, kernelargs)
-        print("Gaussian kernel graph constructed in {} seconds".format(time.time() - subtic))
-    else:
-        del d
-        graph = neighbor_graph(kernel, kernelargs)
-        print("Jaccard graph constructed in {} seconds".format(time.time() - subtic))
-    if not directed:
-        if not prune:
-            # symmetrize graph by averaging with transpose
-            sg = (graph + graph.transpose()).multiply(.5)
-        else:
-            # symmetrize graph by multiplying with transpose
-            sg = graph.multiply(graph.transpose())
-        # retain lower triangle (for efficiency)
-        graph = sp.tril(sg, -1)
-    return runlouvain_given_graph(graph, level_to_return=-1,
-            q_tol=q_tol, louvain_time_limit=louvain_time_limit,
-            min_cluster_size=min_cluster_size, tic=tic)
-
-
 def runlouvain_given_graph(graph, level_to_return, q_tol, louvain_time_limit,
                            min_cluster_size, max_clusters=-1,
                            contin_runs=20, tic=None,
                            seed=1234):
-    if (not isinstance(graph,sp.coo_matrix)):
+    if (not sp.isspmatrix_coo(graph)):
         graph = sp.coo_matrix(graph) 
     # write to file with unique id
     uid = uuid.uuid1().hex
@@ -147,7 +61,7 @@ def runlouvain_average_runs_given_graph(
         graph, n_runs, level_to_return, parallel_threads, verbose,
         max_clusters=-1, tic=None, seed=1234):
 
-    if (not isinstance(graph,sp.coo_matrix)):
+    if (not sp.isspmatrix_coo(graph)):
         graph = sp.coo_matrix(graph) 
     # write to file with unique id
     uid = uuid.uuid1().hex
