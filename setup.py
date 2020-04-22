886989789274b995856f587fd180bb957bc2d14c
Av Shrikumar <avanti.shrikumar@gmail.com>
2020-04-22 04:17:51 -0700

    dependency fix for leidenalg and tqdm

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__== '__main__':
           description='TF MOtif Discovery from Importance SCOres',
           long_description="""Algorithm for discovering consolidated patterns from base-pair-level importance scores""",
           url='https://github.com/kundajelab/tfmodisco',
-          version='0.5.6.0',
+          version='0.5.6.1',
           packages=find_packages(),
           package_data={
                 '': ['cluster/phenograph/louvain/*convert*', 'cluster/phenograph/louvain/*community*', 'cluster/phenograph/louvain/*hierarchy*']
@@ -15,7 +15,8 @@ if __name__== '__main__':
           setup_requires=[],
           install_requires=['numpy>=1.9', 'joblib>=0.11', 
                             'scikit-learn>=0.19',
-                            'h5py>=2.5'],
+                            'h5py>=2.5', 'leidenalg>=0.7.0',
+                            'tqdm>=4.41.1'],
           extras_require={
             'tensorflow': ['tensorflow>=1.7'],
             'tensorflow with gpu': ['tensorflow-gpu>=1.7']},
