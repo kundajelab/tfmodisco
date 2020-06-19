deb9880ff8845bfcc3657a266e1a857059b31fd7
AvantiShri <avanti.shrikumar@gmail.com>
2020-06-19 11:26:57 -0700

    support for sparse return

--- modisco/affinitymat/core.py
+++ modisco/affinitymat/core.py
@@ -8,6 +8,7 @@ import sys
 import time
 import itertools
 import scipy.stats
+import gc
 from joblib import Parallel, delayed
 
 
@@ -312,7 +313,8 @@ class AffmatFromSeqletsWithNNpairs(object):
         self.pattern_comparison_settings = pattern_comparison_settings 
         self.sim_metric_on_nn_pairs = sim_metric_on_nn_pairs
 
-    def __call__(self, seqlets, filter_seqlets=None, seqlet_neighbors=None):
+    def __call__(self, seqlets, filter_seqlets=None,
+                       seqlet_neighbors=None, return_sparse=False):
         (all_fwd_data, all_rev_data) =\
             modiscocore.get_2d_data_from_patterns(
                 patterns=seqlets,
@@ -338,32 +340,56 @@ class AffmatFromSeqletsWithNNpairs(object):
                      neighbors_of_things_to_scan=seqlet_neighbors,
                      filters=filters_all_fwd_data,
                      things_to_scan=all_fwd_data,
-                     min_overlap=self.pattern_comparison_settings.min_overlap) 
+                     min_overlap=self.pattern_comparison_settings.min_overlap,
+                     return_sparse=return_sparse) 
         if (filters_all_rev_data is not None):
             affmat_rev = self.sim_metric_on_nn_pairs(
-                     neighbors_of_things_to_scan=seqlet_neighbors,
-                     filters=filters_all_rev_data,
-                     things_to_scan=all_fwd_data,
-                     min_overlap=self.pattern_comparison_settings.min_overlap) 
+                 neighbors_of_things_to_scan=seqlet_neighbors,
+                 filters=filters_all_rev_data,
+                 things_to_scan=all_fwd_data,
+                 min_overlap=self.pattern_comparison_settings.min_overlap,
+                 return_sparse=return_sparse) 
         else:
             affmat_rev = None
-        if (len(affmat_fwd.shape)==3):
-            #dims are N x N x 2, where first entry of last idx is sim,
-            # and the second entry is the alignment.
-            if (affmat_rev is None):
-                affmat = affmat_fwd
-            else:
-                #will return something that's N x N x 3, where the third
-                # entry in last dim is is_fwd 
-                is_fwd = (affmat_fwd[:,:,0] > affmat_rev[:,:,0])*1.0
-                affmat = np.zeros((affmat_fwd.shape[0],affmat_fwd.shape[1],3))
-                affmat[:,:,0:2] = (affmat_fwd*is_fwd[:,:,None]
-                                   + affmat_rev*(1-is_fwd[:,:,None]))
-                affmat[:,:,2] = is_fwd 
+        if (return_sparse==False):
+            if (len(affmat_fwd.shape)==3):
+                #dims are N x N x 2, where first entry of last idx is sim,
+                # and the second entry is the alignment.
+                if (affmat_rev is None):
+                    affmat = affmat_fwd
+                else:
+                    #will return something that's N x N x 3, where the third
+                    # entry in last dim is is_fwd 
+                    is_fwd = (affmat_fwd[:,:,0] > affmat_rev[:,:,0])*1.0
+                    affmat = np.zeros((affmat_fwd.shape[0],
+                                       affmat_fwd.shape[1],3))
+                    affmat[:,:,0:2] = (affmat_fwd*is_fwd[:,:,None]
+                                       + affmat_rev*(1-is_fwd[:,:,None]))
+                    affmat[:,:,2] = is_fwd 
 
+            else:
+                affmat = (np.maximum(affmat_fwd, affmat_rev) if
+                          (affmat_rev is not None) else np.array(affmat_fwd))
         else:
-            affmat = (np.maximum(affmat_fwd, affmat_rev) if
-                      (affmat_rev is not None) else np.array(affmat_fwd))
+            if (len(affmat_fwd[0].shape)==2):
+                #dims are N x N x 2, where first entry of last idx is sim,
+                # and the second entry is the alignment.
+                if (affmat_rev is None):
+                    affmat = affmat_fwd
+                else:
+                    affmat = [] 
+                    for fwd, rev in zip(affmat_fwd, affmat_rev):
+                        is_fwd = (fwd[:,0] > rev[:,0])*1.0 
+                        new_row = np.zeros((fwd.shape[0],3))
+                        new_row[:,0:2] = (fwd*is_fwd[:,None] +
+                                          rev*(1-is_fwd[:,None]))
+                        new_row[:,2] = is_fwd 
+                        affmat.append(new_row)
+            else:
+                affmat = ([np.maximum(x,y) for (x,y)
+                           in zip(affmat_fwd, affmat_rev)]
+                          if affmat_rev is not None else affmat_fwd)
+                 
         return affmat  
 
 
@@ -385,7 +411,8 @@ class ParallelCpuCrossMetricOnNNpairs(AbstractSimMetricOnNNpairs):
         self.verbose = verbose
 
     def __call__(self, filters, things_to_scan, min_overlap,
-                       neighbors_of_things_to_scan=None):
+                       neighbors_of_things_to_scan=None,
+                       return_sparse=False):
         if (neighbors_of_things_to_scan is None):
             neighbors_of_things_to_scan = [list(range(len(filters)))
                                            for x in things_to_scan] 
@@ -407,24 +434,19 @@ class ParallelCpuCrossMetricOnNNpairs(AbstractSimMetricOnNNpairs):
         # only returns the best similarity and not the alignment that
         # gives rise to that similarity 
         if (self.cross_metric_single_region.returns_pos==False):
-            to_return = np.zeros((things_to_scan.shape[0], filters.shape[0]))
+            if (return_sparse==False):
+                to_return = np.zeros((things_to_scan.shape[0],
+                                      filters.shape[0]))
         else:
-            #each return value will contain both the position of the alignment
-            # as well as the similarity at that position; hence the
-            # length of the third dimension is 2.
-            # The similarity comes first, then the position
-            to_return = np.zeros((things_to_scan.shape[0],
-                                  filters.shape[0], 2))
+            if (return_sparse==False):
+                #each return value will contain both the
+                # position of the alignment
+                # as well as the similarity at that position; hence the
+                # length of the third dimension is 2.
+                # The similarity comes first, then the position
+                to_return = np.zeros((things_to_scan.shape[0],
+                                      filters.shape[0], 2))
 
-        #job_arguments = []
-
-        #for neighbors_of_thing_to_scan, thing_to_scan\
-        #    in zip(neighbors_of_things_to_scan, things_to_scan): 
-        #    args = (filters[neighbors_of_thing_to_scan], thing_to_scan) 
-        #    job_arguments.append(args)
-
-        #print("cp5"); print_memory_use();        
-         
         start = time.time()
         if (self.verbose):
             print("Launching nearest neighbors affmat calculation job")
@@ -437,22 +459,44 @@ class ParallelCpuCrossMetricOnNNpairs(AbstractSimMetricOnNNpairs):
                         things_to_scan[i])
                         for i in range(len(things_to_scan))))
 
+        assert len(results)==len(neighbors_of_things_to_scan)
+        if (self.cross_metric_single_region.returns_pos==False):
+            assert all([len(x)==len(y) for x,y in
+                        zip(results, neighbors_of_things_to_scan)])
+        else:
+            assert all([len(x[0])==len(y) for x,y in
+                        zip(results, neighbors_of_things_to_scan)])
+
         if (self.verbose):
             print("Parallel runs completed")
             print_memory_use()
             sys.stdout.flush()
 
-        for (thing_to_scan_idx, (result, thing_to_scan_neighbor_indices))\
-             in enumerate(zip(results, neighbors_of_things_to_scan)):
-            #adjust the "position" to remove the effect of the padding
-            if (self.cross_metric_single_region.returns_pos==True):
-                result[1] -= padding_amount 
-                to_return[thing_to_scan_idx,
-                          thing_to_scan_neighbor_indices] =\
-                    np.transpose(result,(1,0))
+        if (return_sparse==True):
+            to_return = []
+            if (self.cross_metric_single_region.returns_pos==False):
+                assert len(results[0].shape)==1  
+                to_return = results
             else:
-                to_return[thing_to_scan_idx,
-                          thing_to_scan_neighbor_indices] = result
+                assert len(results[0].shape)==2 
+                for result in results:
+                    #adjust the "position" to remove the effect of the padding
+                    result[1] -= padding_amount 
+                    to_return.append(np.transpose(result, (1,0)))
+        else: 
+            for (thing_to_scan_idx, (result, thing_to_scan_neighbor_indices))\
+                 in enumerate(zip(results, neighbors_of_things_to_scan)):
+                #adjust the "position" to remove the effect of the padding
+                if (self.cross_metric_single_region.returns_pos==True):
+                    result[1] -= padding_amount 
+                    to_return[thing_to_scan_idx,
+                              thing_to_scan_neighbor_indices] =\
+                        np.transpose(result,(1,0))
+                else:
+                    to_return[thing_to_scan_idx,
+                              thing_to_scan_neighbor_indices] = result
+
+        gc.collect()
 
         end = time.time()
         if (self.verbose):
