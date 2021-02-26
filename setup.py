d4c305c5074ad27281cd22b0b73365ede3ac7fa4
Av Shrikumar <avanti.shrikumar@gmail.com>
2021-02-26 15:48:30 -0800

    fixed n_jobs bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__== '__main__':
           description='TF MOtif Discovery from Importance SCOres',
           long_description="""Algorithm for discovering consolidated patterns from base-pair-level importance scores""",
           url='https://github.com/kundajelab/tfmodisco',
-          version='0.5.13.1',
+          version='0.5.13.2',
           packages=find_packages(),
           package_data={
                 '': ['cluster/phenograph/louvain/*convert*', 'cluster/phenograph/louvain/*community*', 'cluster/phenograph/louvain/*hierarchy*']
