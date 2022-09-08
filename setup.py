707583b1138c34a25f3c05874d5a0f5db9f79651
Jacob Schreiber <jmschreiber91@gmail.com>
2022-09-08 12:23:56 -0700

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='modiscolite',
-	version='0.0.1',
+	version='0.0.2',
 	author='Jacob Schreiber',
 	author_email='jmschreiber91@gmail.com',
 	packages=['modiscolite'],
