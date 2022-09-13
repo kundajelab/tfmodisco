f856bffe15d33a89211fe12d366c93cf355840ae
Jacob Schreiber <jmschreiber91@gmail.com>
2022-09-12 17:05:44 -0700

    FIX mdoisco h5py issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='modiscolite',
-	version='0.0.4',
+	version='0.0.5',
 	author='Jacob Schreiber',
 	author_email='jmschreiber91@gmail.com',
 	packages=['modiscolite'],
